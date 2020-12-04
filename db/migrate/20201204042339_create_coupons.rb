994c69d4fcbf0d934de99ecbcd6460ea54cdf8c8
jose luis <cortezluisjose@gmail.com>
2020-12-04 04:24:05 +0000

    Automatically backed up by Learn

--- db/migrate/20201204042339_create_coupons.rb
+++ db/migrate/20201204042339_create_coupons.rb
@@ -2,7 +2,9 @@ class CreateCoupons < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |t|
       
-      
+       t.string :coupon_code
+            t.string :store
+        end
     end
   end
-end
+
